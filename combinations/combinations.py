d1bff0b12743f6025f4f94fdbcb5fe93ddab3e78
saviaga <saviaga@gmail.com>
2021-02-07 16:34:06 -0500

    Time: 464 ms (64.96%), Space: 15.9 MB (24.92%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,22 +1,25 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
-        def backtracking(first,chosen):
-            if len(chosen)==k:
+        
+        def combineHelper(first,chosen):
+            
+            if len(chosen) == k:
                 return res.append(chosen[:])
-            else:
-                for i in range(first,n+1):
-                    #choose                    
-                    chosen.append(i)
-                    #explore
-                    backtracking(i+1,chosen)
-                    #unchose
-                    chosen.pop()
+            
+            for i in range(first, n+1):
+                #chose
+                chosen.append(i)
+                #explore
+                combineHelper(i+1,chosen)
+                #unchose
+                chosen.pop()
+                
+            
+            
             
         res = []
-        backtracking(1,[])
+        combineHelper(1,[])
         return res
-            
-        
         
         
