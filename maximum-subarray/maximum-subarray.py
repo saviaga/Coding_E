f94c9d825a5e5381f84d73a71a9c0e3bdb9c7df2
saviaga <saviaga@gmail.com>
2021-02-23 18:27:55 -0500

    Time: 48 ms (99.95%), Space: 14.8 MB (82.12%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,16 +1,14 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums)==1:
-            return nums[0]
-        
-        run_sum = [nums[0]]
-        
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #edge cases
+        max_current = nums[0]
+        max_global = nums[0]
         for i in range(1,len(nums)):
-            run_sum.append(max(nums[i],run_sum[i-1]+nums[i]))
-                      
-        return max(run_sum)
+            max_current = max(nums[i],max_current+nums[i])
+            if max_current > max_global:
+                max_global = max_current
+        return max_global
+            
+            
+        
         
