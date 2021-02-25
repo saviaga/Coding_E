92b6ec09ad5417489d9009b476e381cb04a05b7a
saviaga <saviaga@gmail.com>
2021-02-24 20:27:07 -0500

    Time: 84 ms (74.72%), Space: 15.4 MB (82.68%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,45 @@
-class Solution(object):
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
         
+        def find_first_ocurrence(nums,target):
+            left =0
+            right = len(nums)-1
+            res = -1
+            while left <= right:
+                mid = left + (right - left)//2
+                
+                if nums[mid] == target:
+                    res = mid
+                    
+                    right = mid-1
+                    
+                    
+                elif nums[mid] > target:
+                    right = mid -1
+                
+                else:
+                    left = mid + 1
+            return res
         
-        position = [-1,-1]
+        def find_last_ocurrence(nums,target):
+            left =0
+            right = len(nums)-1
+            res = -1
+            while left <= right:
+                mid = left + (right - left)//2
+                
+                if nums[mid] == target:
+                    res = mid
+                    left = mid +1
+                    
+                elif nums[mid] > target:
+                    right = mid -1
+                
+                else:
+                    left = mid + 1
+            return res
         
-        for i in range(len(nums)):
-            if nums[i]==target:
-                position[0] =i
-                break
+        return [find_first_ocurrence(nums,target),find_last_ocurrence(nums,target)]
         
                 
-        for i in reversed(range(len(nums))):
-            if nums[i]==target:
-                position[1] =i
-                break
-            
-        return position
         
