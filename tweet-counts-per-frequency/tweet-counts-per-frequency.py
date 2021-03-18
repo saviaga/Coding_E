047e2e68bd1636c4a08133571cd90546ebb850a1
saviaga <saviaga@gmail.com>
2021-03-18 17:06:08 -0400

    Time: 416 ms (33.25%), Space: 21.5 MB (81.23%) - LeetHub

--- tweet-counts-per-frequency/tweet-counts-per-frequency.py
+++ tweet-counts-per-frequency/tweet-counts-per-frequency.py
@@ -1,15 +1,12 @@
 class TweetCounts:
 
     def __init__(self):
-        self.dict_tweets = defaultdict(list)
-        self.count=0
-
-    def recordTweet(self, tweetName: str, time: int) -> None:        
-            self.dict_tweets[tweetName].append(time)
-
-            
-            
+        self.tweets = defaultdict(list)
         
+
+    def recordTweet(self, tweetName: str, time: int) -> None:
+        self.tweets[tweetName].append(time)
+        self.tweets[tweetName].sort()
 
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
         if freq == 'minute':
@@ -20,20 +17,14 @@ class TweetCounts:
             
         if freq == 'day':
             seconds = 86400
+       
+        ans = [0] * (((endTime - startTime)//seconds)+1)
         
-        ordered_times = self.dict_tweets[tweetName]
-        ordered_times.sort()
-    #    print(ordered_times)
-    #    print(startTime,endTime)
-
-        ans = [0] * ((endTime - startTime)//seconds+1)
-    #    print(ans)
-        for t in self.dict_tweets[tweetName]:
-     #       print(tweetName)
-     #       print("dict",self.dict_tweets[tweetName])
-            if startTime <= t <= endTime: ans[(t-startTime)//seconds] += 1
-     #   print("ans",ans)
-        return ans   
+        for t in self.tweets[tweetName]:
+            if startTime <= t <= endTime: 
+                ans[(t-startTime)//seconds] += 1
+        return ans         
+        
         
 
 
