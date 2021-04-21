a10f4cfb9f1da9aa16d968eaebf26537bac7d4ea
saviaga <saviaga@gmail.com>
2021-04-21 10:17:59 -0400

    Time: 64 ms (62.90%), Space: 14.1 MB (93.39%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,33 +1,22 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if s==' ':
-            return 1
-        
-        longest = 0
-        substring = set()
-        left,right=0,0
-        
-        while right<len(s):
-            
-            #if equal and it is not in the set add it
+    def lengthOfLongestSubstring(self, str1: str) -> int:
+          window_start = 0
+          max_length = 0
+          char_index_map = {}
 
-            while (s[right] in substring):
-                    substring.remove(s[left])
-                    left +=1
-            substring.add(s[right])
-            longest = max(longest,right-left+1)
-            right+=1
-
-                
-        
-        return longest
-            
-        
-        #["p,w,w,k,e,w"]
-# left         ^ 
-#right         ^    
-#longest=2
-#.         ()
-        
+          # try to extend the range [windowStart, windowEnd]
+          for window_end in range(len(str1)):
+            right_char = str1[window_end]
+            # if the map already contains the 'right_char', shrink the window from the beginning so that
+            # we have only one occurrence of 'right_char'
+            if right_char in char_index_map:
+              # this is tricky; in the current window, we will not have any 'right_char' after its previous index
+              # and if 'window_start' is already ahead of the last index of 'right_char', we'll keep 'window_start'
+              window_start = max(window_start, char_index_map[right_char] + 1)
+            # insert the 'right_char' into the map
+            char_index_map[right_char] = window_end
+            # remember the maximum length so far
+            max_length = max(max_length, window_end - window_start + 1)
+          return max_length
        
         
