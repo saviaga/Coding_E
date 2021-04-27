49deb84d874980df5f6d5e151ecb4633a5f5a1d7
saviaga <saviaga@gmail.com>
2021-04-27 10:59:29 -0400

    Time: 128 ms (63.05%), Space: 15.2 MB (34.66%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,29 +1,33 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        ns, np = len(s), len(p)
-        if ns < np:
-            return []
-
-        p_count = Counter(p)
-        s_count = Counter()
-        output = []
-        # sliding window on the string s
-        for i in range(ns):
-            # add one more letter 
-            # on the right side of the window
-            s_count[s[i]] += 1
-            print(s_count)
-            # remove one letter 
-            # from the left side of the window
-            if i >= np:
-                if s_count[s[i - np]] == 1:
-                    del s_count[s[i - np]]
-                else:
-                    s_count[s[i - np]] -= 1
-            # compare array in the sliding window
-            # with the reference array
-            if p_count == s_count:
-                output.append(i - np + 1)
         
-        return output    
-
+        start,matched =0,0
+        indices = []
+        #create counter with the pattern
+        dict_pattern = collections.Counter(p)
+        
+        #go through the string
+        for end in range(len(s)):
+            right_char = s[end]
+            
+            if right_char in dict_pattern:
+                dict_pattern[right_char] -=1
+                
+                if dict_pattern[right_char] ==0:
+                    matched +=1
+            
+            if matched == len(dict_pattern):
+                indices.append(start)
+                
+            if end>=len(p)-1:
+                left_char = s[start]
+                if left_char in dict_pattern:
+                    if dict_pattern[left_char]==0:
+                        matched -=1
+                    dict_pattern[left_char]+=1
+                start+=1
+        return indices
+                    
+                
+                
+        
