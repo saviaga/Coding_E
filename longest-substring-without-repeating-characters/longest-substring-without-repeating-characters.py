1a9ad65f5a0bf92f1cc06d9cb6979dff8952da21
saviaga <saviaga@gmail.com>
2021-04-27 12:11:38 -0400

    Time: 60 ms (72.74%), Space: 14.3 MB (80.47%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,11 +5,12 @@ class Solution:
         start = 0
         window_size = 0
         for end in range(len(nums)):
-            right_num = nums[end]
-            if right_num  in dict_char:
-                start = max(start,dict_char[right_num]+1)
-            dict_char[right_num] = end
+            right_char = nums[end]
+            if right_char in dict_char:
+                start = max(start,dict_char[right_char]+1)
+            dict_char[right_char] = end
             max_lenght = max(max_lenght,end-start+1)
+            
 
         return max_lenght
                 
