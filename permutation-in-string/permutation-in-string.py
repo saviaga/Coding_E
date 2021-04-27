55e42b1ed39eab6d795de9e8511d0c13b53bd1d5
saviaga <saviaga@gmail.com>
2021-04-27 10:32:38 -0400

    Time: 80 ms (52.43%), Space: 14.2 MB (86.62%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -1,16 +1,40 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def checkInclusion(self, pattern: str, s1: str) -> bool:
+        start = 0
+        matched = 0
+        #create hashmap from the dictionary
+        dict_pattern = collections.Counter(pattern)
         
-        dic_s1 = collections.Counter(s1)        
-        start=0
-        end=len(s1)-1
-        while end < len(s2):
+        #go through the window
+        for end in range(len(s1)):
+            right_char = s1[end]
             
-            dic_temp =  collections.Counter(list(s2[start:end+1]))
-            if dic_s1==dic_temp:
-                return True  
-            start+=1
-            end+=1
+            #if there is a matching character in pattern
+            #decrease the pattern hashmap in the char
+            
+            if right_char in dict_pattern:
+                dict_pattern[right_char] -=1
+            
+                #increase the matched counter if the dict[char] ==0
+                #cause maybe a pattern has the same char twice?
+                if dict_pattern[right_char] == 0:
+                    matched +=1
+                        
+            if matched == len(dict_pattern):
+                return True
+            
+            #decrease window
+            if end >= len(pattern)-1:
+                left_char = s1[start]
+                if left_char in dict_pattern:
+                    if dict_pattern[left_char] == 0:
+                        matched -= 1
                 
+                    dict_pattern[left_char] +=1
+                start+=1
+            
         return False
             
+       
+        
+        
