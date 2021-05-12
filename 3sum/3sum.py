5e977ffd1dc139897d93e2bcc1fdd7cf6ce32ca9
saviaga <saviaga@gmail.com>
2021-05-12 09:39:54 -0400

    Time: 4444 ms (16.59%), Space: 18 MB (31.65%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,31 +1,35 @@
 class Solution:
     def threeSum(self, arr: List[int]) -> List[List[int]]:
         
-        def search_pair(arr, target_sum, left, triplets):
-            right = len(arr) - 1
-            while(left < right):
-                current_sum = arr[left] + arr[right]
+        def search_pair(arr, target_sum, start, triplets):
+            end = len(arr) - 1
+            
+            while(start < end):
+                current_sum = arr[start] + arr[end]
                 if current_sum == target_sum:  # found the triplet
-                    new_triplet =[-target_sum, arr[left], arr[right]] 
+                    new_triplet =[-target_sum, arr[start], arr[end]] 
                     if new_triplet not in triplets:
                         triplets.append(new_triplet)
-                    left += 1
-                    right -= 1
-                    while left < right and arr[left] == arr[left - 1]:
-                        left += 1  # skip same element to avoid duplicate triplets
-                    while left < right and arr[right] == arr[right + 1]:
-                        right -= 1  # skip same element to avoid duplicate triplets
+                    start += 1
+                    end -= 1
+                    while start < end and arr[start] == arr[start - 1]:
+                        start += 1  # skip same element to avoid duplicate triplets
+                    while start < end and arr[end] == arr[end + 1]:
+                        end -= 1  # skip same element to avoid duplicate triplets
                 elif target_sum > current_sum:
-                    left += 1  # we need a pair with a bigger sum
+                    start += 1  # we need a pair with a bigger sum
                 else:
-                    right -= 1  # we need a pair with a smaller sum
+                    end -= 1  # we need a pair with a smaller sum
 
             
         #We sort the array
         arr.sort()
         triplets = []
         for i in range(len(arr)):
+            #y+z = -x
+            #grab first element x (arr[i]), then try to fidn the next two: y + z
             search_pair(arr, -arr[i], i+1, triplets)
+            
         return triplets
             
 
